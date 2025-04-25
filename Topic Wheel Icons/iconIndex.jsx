6e1b96e03d74971c50180bd731c383ac921e1777
LawrenceMoore <moorelawrence.m@gmail.com>
2025-04-25 18:01:39 +1200

    added in iconTypes github ref

--- Topic Wheel Icons/iconIndex.jsx
+++ Topic Wheel Icons/iconIndex.jsx
@@ -1,83 +1,37 @@
 //  <-- MUST DO ON UPDATE!! --> 
-// 1. transpile command: npx babel --presets=@babel/preset-env,@babel/preset-react MyNewWormhole.jsx -o MyNewWormhole.js
+// 1. transpile command: npx babel --presets=@babel/preset-env,@babel/preset-react iconIndex.jsx -o iconIndex.js
 // 2. add, commit, push to main
 
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
+import { iconTypes } from './iconTypes';
 
-export default function MyNewWormhole({request, callback}) {
-    const [inputValue, setInputValue] = useState('');
-    const handlePress = () => {
-        if (callback) {
-          callback(inputValue);
-        }
-    }
-    if (request.length == 0) {
-        return (
-            <TouchableOpacity onPress={handlePress} style={styles.container}>
-            <View style={styles.container}>
-                <Text style={styles.title}>No request available</Text>
-            </View>
-            </TouchableOpacity>
-        );
-    }
-    return (
-        <TouchableOpacity onPress={handlePress} style={styles.container}>
-            <View style={styles.card}>
-                <Text style={styles.title}>{request.requestThings[0].properties.title}</Text>
-                <Text style={styles.description}>{request.requestThings[0].properties.description}</Text>
-                <Text style={styles.price}>Base Price: ${request.requestThings[0].properties.basePricePerNight}</Text>
-                <Text style={styles.location}>
-                    Location: {request.requestThings[0].properties.location?.address?.city}, {request.requestThings[0].properties.location?.address?.state}
-                </Text>
-                <Text style={styles.type}>Type: {request.requestThings[0].header.type}</Text>
-            </View>
-        </TouchableOpacity>
-    );
-}
+const RemoteIcon = (props) => {
+  const selectedIcon = iconTypes[props.type] ?? iconTypes['check'];
+  const assetSource = props.full && selectedIcon.fullAsset 
+    ? { uri: selectedIcon.fullAsset } 
+    : { uri: selectedIcon.asset };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f8f8f8',
-        padding: 10,
-    },
-    card: {
-        padding: 20,
-        marginVertical: 10,
-        backgroundColor: '#fff',
-        borderRadius: 8,
-        shadowColor: '#000',
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 3,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    description: {
-        fontSize: 14,
-        marginVertical: 5,
-    },
-    price: {
-        fontSize: 16,
-        fontWeight: '600',
-        marginVertical: 5,
-    },
-    location: {
-        fontSize: 14,
-        color: '#555',
-    },
-    type: {
-        fontSize: 18,
-        color: '#777',
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 5,
-        paddingHorizontal: 10,
-    },
-});
+  const styles = StyleSheet.create({
+    icon: {
+      ...props.style,
+      display: 'flex',
+      width: props.size * selectedIcon.ratio.width,
+      height: props.size * selectedIcon.ratio.height,
+      minWidth: props.size * selectedIcon.ratio.width,
+      minHeight: props.size * selectedIcon.ratio.height,
+      maxWidth: props.size * selectedIcon.ratio.width,
+      maxHeight: props.size * selectedIcon.ratio.height,
+    }
+  });
+
+  return (
+    <ImageBackground
+      style={styles.icon}
+      source={assetSource}
+      resizeMode="contain"
+    />
+  );
+};
+
+export default RemoteIcon;
---
+++ Topic Wheel Icons/iconTypes.js
@@ -0,0 +1,220 @@
+export const iconTypes = {
+    arrowRight: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/arrowRight.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      back: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/back.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      backWhite: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/arrowBack24Px.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      bankline: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/bankline2X.png',
+        // fullAsset: require('./bankline2X.png'),
+        ratio: {
+          width: 0.9310344828,
+          height: 1
+        }
+      },
+      certificate: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-certificate2X.png',
+        // fullAsset: require('./g-certificate2X.png'),
+        ratio: {
+          width: 1,
+          height: 0.9473684211
+        }
+      },
+      check: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-check2X.png',
+        // fullAsset: require('./g-check2X.png'),
+        ratio: {
+          width: 1,
+          height: 0.5
+        }
+      },
+      close: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/close.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      dollar: {
+        // asset: require('./dolla2X.png'),
+        fullAsset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/dolla2X.png',
+        ratio: {
+          width: 1,
+          height: 0.511627907
+        }
+      },
+      faqLine: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/faqLine2X.png',
+        // fullAsset: require('./faqLine2X.png'),
+        ratio: {
+          width: 1,
+          height: 0.8125
+        }
+      },
+      globe: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-globe2X.png',
+        ratio: {
+          width: 0.8888888889,
+          height: 1
+        }
+      },
+      group: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-group.png',
+        // fullAsset: require('./g-group.png'),
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      info: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/info24Px.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      loan: {
+        // asset: require('./loan2X.png'),
+        fullAsset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/loan2X.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      logoIcon: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/logoIcon2X.png',
+        // fullAsset: require('./g-logoIcon2X.png'),
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      mma: {
+        // asset: require('./mma2X.png'),
+        fullAsset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/mma2X.png',
+        ratio: {
+          width: 1,
+          height: 0.8823529412
+        }
+      },
+      mobilemoney: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-mobilemoney2X.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      moneyorder: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-moneyorder2X.png',
+        ratio: {
+          width: 1,
+          height: 0.7333333333
+        }
+      },
+      peeps: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-peeps2X.png',
+        // fullAsset: require('./peeps2X.png'),
+        ratio: {
+          width: 1,
+          height: 0.5679012346
+        }
+      },
+      phoneLine: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/phoneLine2X.png',
+        // fullAsset: require('./phoneLine2X.png'),
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      retire: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-retire2X.png',
+        // fullAsset: require('./g-retire2X.png'),
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      savings: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-savings2X.png',
+        // fullAsset: require('./g-savings2X.png'),
+        ratio: {
+          width: 1,
+          height: 0.8260869565
+        }
+      },
+      selectLeft: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/selectLeft2X.png',
+        ratio: {
+          width: 0.5333333333,
+          height: 1
+        }
+      },
+      selectRight: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/selectRight2X.png',
+        ratio: {
+          width: 0.5333333333,
+          height: 1
+        }
+      },
+      smallbiz: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-smallbiz2X.png',
+        fullAsset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/smallbiz2X.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      solidArrow: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/triangle.png',
+        ratio: {
+          width: 0.375,
+          height: 1
+        }
+      },
+      star: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/fave2X.png',
+        ratio: {
+          width: 1,
+          height: 0.9375
+        }
+      },
+      transfermoney: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-transfermoney2X.png',
+        ratio: {
+          width: 1,
+          height: 1
+        }
+      },
+      usa: {
+        asset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/g-usa2X.png',
+        ratio: {
+          width: 1,
+          height: 0.75
+        }
+      },
+      wire: {
+        // asset: require('./wire2X.png'),
+        fullAsset: 'https://raw.githubusercontent.com/needseek/NS-Wormholes/main/Topic%20Wheel%20Icons/icons/wire2X.png',
+        ratio: {
+          width: 1,
+          height: 0.652173913
+        }
+    }, 
+};
