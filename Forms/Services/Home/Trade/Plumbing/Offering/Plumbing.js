14cabd7493b783ecbf0656b4584efb6d5feb8cc5
LawrenceMoore <moorelawrence.m@gmail.com>
2025-05-02 06:32:16 +1000

    force

--- Forms/Services/Home/Trade/Plumbing/Offering/Plumbing.js
+++ Forms/Services/Home/Trade/Plumbing/Offering/Plumbing.js
@@ -974,7 +974,7 @@ var PlumbingForm = function PlumbingForm() {
     }]
   }, /*#__PURE__*/_react["default"].createElement(_reactNative.Text, {
     style: styles.label
-  }, "fed force Entity Type", /*#__PURE__*/_react["default"].createElement(_reactNative.Text, {
+  }, "forced change Entity Type", /*#__PURE__*/_react["default"].createElement(_reactNative.Text, {
     style: styles.requiredStar
   }, "*")), /*#__PURE__*/_react["default"].createElement(DropDownPicker, {
     open: false,
