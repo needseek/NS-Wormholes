d10f5f8b1f363f844f16e5eec34ed7ac824bc430
LawrenceMoore <moorelawrence.m@gmail.com>
2025-05-20 05:58:20 +1000

    adding icons

--- Topic Wheel Icons/RemoteIcon.jsx
+++ Topic Wheel Icons/RemoteIcon.jsx
@@ -6,6 +6,8 @@ import React, { useState, useEffect } from 'react';
 import { ImageBackground, StyleSheet } from 'react-native';
 
 const iconTypes = {
+
+  // ----------- HELPER ICONS ------------
   arrowRight: {
     asset: "https://i.imgur.com/frEP0aa.png",
     ratio: {
@@ -27,137 +29,11 @@ const iconTypes = {
       height: 1
     }
   },
-  bankline: {
-    asset: "https://i.imgur.com/4lzTAhT.png",
-    ratio: {
-      width: 0.9310344828,
+  solidArrow: {
+    asset: "https://i.imgur.com/NrcxyQ4.png",
+    ratio: {  
+      width: 0.375,
       height: 1
-    }
-  },
-  certificate: {
-    asset: "https://i.imgur.com/CZCXZvr.png",
-    ratio: {
-      width: 1,
-      height: 0.9473684211
-    }
-  },
-  check: {
-    asset: "https://i.imgur.com/FC2oVzq.png",
-    ratio: {
-      width: 1,
-      height: 0.5
-    }
-  },
-  close: {
-    asset: "https://i.imgur.com/ciZZkR5.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  dollar: {
-    fullAsset: "https://i.imgur.com/1FkRVH3.png",
-    ratio: {
-      width: 1,
-      height: 0.511627907
-    }
-  },
-  faqLine: {
-    asset: "https://i.imgur.com/qP8Ipyy.png",
-    ratio: {
-      width: 1,
-      height: 0.8125
-    }
-  },
-  globe: {
-    asset: "https://i.imgur.com/FRJduiJ.png",
-    ratio: {
-      width: 0.8888888889,
-      height: 1
-    }
-  },
-  group: {
-    asset: "https://i.imgur.com/CspcaQ8.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  home: {
-    fullAsset: "https://i.imgur.com/HKCwwfQ.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  info: {
-    asset: "https://i.imgur.com/uW9C6YO.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  loan: {
-    fullAsset: "https://i.imgur.com/3t32coA.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  logoIcon: {
-    asset: "https://i.imgur.com/ypo1GAY.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  mma: {
-    fullAsset: "https://i.imgur.com/e7wlYD1.png",
-    ratio: {
-      width: 1,
-      height: 0.8823529412
-    }
-  },
-  mobilemoney: {
-    asset: "https://i.imgur.com/4PzL8xp.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  moneyorder: {
-    asset: "https://i.imgur.com/4OoQPw6.png",
-    ratio: {
-      width: 1,
-      height: 0.7333333333
-    }
-  },
-  peeps: {
-    asset: "https://i.imgur.com/ezDvd9q.png",
-    ratio: {
-      width: 1,
-      height: 0.5679012346
-    }
-  },  
-  phoneLine: {
-    asset: "https://i.imgur.com/z1iAAaC.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  retire: {
-    asset: "https://i.imgur.com/aQsuDBN.png",
-    ratio: {
-      width: 1,
-      height: 1
-    }
-  },
-  savings: {
-    asset: "https://i.imgur.com/3t32coA.png",
-    ratio: {
-      width: 1,
-      height: 0.8260869565
     }
   },
   selectLeft: {
@@ -174,6 +50,215 @@ const iconTypes = {
       height: 1
     }
   },
+
+  // ----------- TOPIC ICONS ------------
+  peeps: {
+    asset: "https://i.imgur.com/ezDvd9q.png",
+    ratio: {
+      width: 1,
+      height: 0.5679012346
+    }
+  }, 
+
+  // ----------- HOME ICONS ------------
+  home: {
+    fullAsset: "https://i.imgur.com/HKCwwfQ.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+
+  // ----------- TRADE ICONS ------------
+  trade: {
+    fullAsset: "https://i.imgur.com/pgJwxrF.png",
+    ratio: {
+      width: 1.2,
+      height: 1.2
+    }
+  }, 
+
+
+  services: {
+    asset: "https://i.imgur.com/LDEF2IS.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  thingsToDo: {
+    asset: "https://i.imgur.com/GkyRUr8.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  commercial: {
+    asset: "https://i.imgur.com/eiThdKt.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  plumber: {
+    fullAsset: "https://i.imgur.com/WEx2eYL.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  carpenter: {
+    fullAsset: "https://i.imgur.com/ZLfjEiM.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  person: {
+    asset: "https://i.imgur.com/Y4pf0bN.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+
+  // not in use
+  bankline: {
+    asset: "https://i.imgur.com/4lzTAhT.png",
+    ratio: {
+      width: 0.9310344828,
+      height: 1
+    }
+  },
+  // not in use
+  certificate: {
+    asset: "https://i.imgur.com/CZCXZvr.png",
+    ratio: {
+      width: 1,
+      height: 0.9473684211
+    }
+  },
+  // not in use
+  check: {
+    asset: "https://i.imgur.com/FC2oVzq.png",
+    ratio: {
+      width: 1,
+      height: 0.5
+    }
+  },
+  // not in use
+  close: {
+    asset: "https://i.imgur.com/ciZZkR5.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  dollar: {
+    fullAsset: "https://i.imgur.com/1FkRVH3.png",
+    ratio: {
+      width: 1,
+      height: 0.511627907
+    }
+  },
+  // not in use
+  faqLine: {
+    asset: "https://i.imgur.com/qP8Ipyy.png",
+    ratio: {
+      width: 1,
+      height: 0.8125
+    }
+  },
+  // not in use
+  globe: {
+    asset: "https://i.imgur.com/FRJduiJ.png",
+    ratio: {
+      width: 0.8888888889,
+      height: 1
+    }
+  },
+  // not in use
+  group: {
+    asset: "https://i.imgur.com/CspcaQ8.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  info: {
+    asset: "https://i.imgur.com/uW9C6YO.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  loan: {
+    fullAsset: "https://i.imgur.com/3t32coA.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  logoIcon: {
+    asset: "https://i.imgur.com/ypo1GAY.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  mma: {
+    fullAsset: "https://i.imgur.com/e7wlYD1.png",
+    ratio: {
+      width: 1,
+      height: 0.8823529412
+    }
+  },
+  // not in use
+  mobilemoney: {
+    asset: "https://i.imgur.com/4PzL8xp.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  moneyorder: {
+    asset: "https://i.imgur.com/4OoQPw6.png",
+    ratio: {
+      width: 1,
+      height: 0.7333333333
+    }
+  },
+  // not in use
+  phoneLine: {
+    asset: "https://i.imgur.com/z1iAAaC.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  retire: {
+    asset: "https://i.imgur.com/aQsuDBN.png",
+    ratio: {
+      width: 1,
+      height: 1
+    }
+  },
+  // not in use
+  savings: {
+    asset: "https://i.imgur.com/3t32coA.png",
+    ratio: {
+      width: 1,
+      height: 0.8260869565
+    }
+  },
+  // not in use
   smallbiz: {
     asset: "https://i.imgur.com/imKesdW.png",
     fullAsset: "https://i.imgur.com/yViaq30.png",
@@ -182,13 +267,7 @@ const iconTypes = {
       height: 1
     }
   },
-  solidArrow: {
-    asset: "https://i.imgur.com/NrcxyQ4.png",
-    ratio: {  
-      width: 0.375,
-      height: 1
-    }
-  },
+  // not in use
   star: {
     asset: "https://i.imgur.com/soQQ0u9.png",
     ratio: {
@@ -196,13 +275,7 @@ const iconTypes = {
       height: 0.9375
     }
   },
-  trade: {
-    fullAsset: "https://i.imgur.com/pgJwxrF.png",
-    ratio: {
-      width: 1.2,
-      height: 1.2
-    }
-  },
+  // not in use
   transfermoney: {
     asset: "https://i.imgur.com/vtwKdtf.png",
     ratio: {
@@ -210,6 +283,7 @@ const iconTypes = {
       height: 1
     }
   },
+  // not in use
   usa: {
     asset: "https://i.imgur.com/xwoMn2t.png",
     ratio: {
@@ -217,6 +291,7 @@ const iconTypes = {
       height: 0.75
     }
   },
+  // not in use
   wire: {
     fullAsset: "https://i.imgur.com/UAjVFyp.png",
     ratio: {
